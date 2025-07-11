d9898063c5bd4ce5b0e0f208c2bab463a0c2a2bb
Seth <sockerman@wisc.edu>
2025-07-11 16:10:12 +0000

    add to use pems dataset in script

--- ddp_testing/runSingleNode.sh
+++ ddp_testing/runSingleNode.sh
@@ -2,13 +2,13 @@
 
 mkdir -p data 
 
-python3 pems_ddp.py -m index -np 4 -g true > output.log 2>&1
+python3 pems_ddp.py -m index -np 4 -g true --dataset pems > output.log 2>&1
 mkdir PemsIndex
 mv *.csv PemsIndex/
 mv output.log PemsIndex/
 rm flag.txt
 
-python3 opt_baseline.py -m dask -np 4 > output.log 2>&1
+python3 opt_baseline.py -m dask -np 4 --dataset pems > output.log 2>&1
 mkdir PemsBase
 mv *.csv PemsBase/
 mv output.log PemsBase/
