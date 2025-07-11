351c323ca45c4ef7f3505123dfcf729fe56eaf6b
Seth <sockerman@wisc.edu>
2025-07-11 15:58:31 +0000

    add downloadcheck method

--- ddp_testing/utils.py
+++ ddp_testing/utils.py
@@ -10,6 +10,32 @@ import psutil
 from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, nvmlShutdown
 from torch.utils.data import Sampler, DataLoader, Dataset
 import math
+import requests
+from tqdm import tqdm
+
+
+def downloadCheck():
+    if not os.path.isdir("data"):
+        raise FileNotFoundError("Error: The 'data/' subdirectory is missing. "
+                                "Scripts assume data and adjacency matrix files will be placed in 'data/'.")
+    else:
+        PeMS_file_links = {
+        "adj_mx_pems.pkl" : "https://anl.app.box.com/shared/static/4143x1repqa1u26aiz7o2rvw3vpcu0wp",
+        "pems.h5": "https://anl.app.box.com/shared/static/7hfhtie02iufy75ac1d8g8530majwci0"
+        }          
+        
+        for key in PeMS_file_links.keys():
+            if not os.path.isfile(os.path.join("./data/",key)):
+                print("Downloading ", key, flush=True)
+                response = requests.get(PeMS_file_links[key], stream=True)
+                file_size = int(response.headers.get('content-length', 0))
+
+                with open(os.path.join("./data/", key), "wb") as file, tqdm(total=file_size, unit="B", unit_scale=True, unit_divisor=1024) as progress_bar:
+                    for chunk in response.iter_content(chunk_size=33554432):
+                        file.write(chunk)
+                        progress_bar.update(len(chunk))
+
+
 
 def masked_mae_loss(y_pred, y_true):
 
